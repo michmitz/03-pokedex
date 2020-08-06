e773e28b7daefc7f574ab230aad9b4e866fb5969
Michelle Stermitz <stermitzm@gmail.com>
2020-08-05 17:23:49 -0700

    Added PokeList and PokeItem components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import Header from './Header.js';
 import './App.css';
+// import request from 'superagent';
 // import PokemonList from './PokemonList.js';
 
-function App() {
+
+
+
+
+
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
       <Header/>
         <div className="input-div">
           <input/>
           <button className="button">Search</button>
+          {/* <PokemonList pokeState={this.state.pokeState}/> */}
         </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/PokemonList.js
+++ src/PokemonList.js
@@ -4,7 +4,9 @@ export default class PokemonList extends Component {
     render() {
         return (
             <div>
-                
+                {
+                this.props.pokeState.map((poke) => <PokemonItem key={poke._id} pokemon={poke.pokemon}/>)
+            }   
             </div>
         )
     }
@@ -13,8 +15,9 @@ export default class PokemonList extends Component {
 export class PokemonItem extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="image-container">
+                <p className="image-title">{this.props.pokeState.pokemon}</p>
+                <img src={this.props.pokeState.url_image} alt=''/>
             </div>
         )
     }
